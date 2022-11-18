427019d234e946695af15ea9d85d9bbe78a8e07f
VaishnaviGhadage <115876140+VaishnaviGhadage@users.noreply.github.com>
2022-11-18 23:32:27 +0530

    Add files via upload

---
+++ AsciiValToAlphabet.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class AsciiValToAlphabet
+{
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the ascii value");
+		int n=sc.nextInt();
+		System.out.println((char)n);
+	}
+}
